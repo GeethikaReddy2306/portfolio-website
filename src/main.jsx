f0633407114e7a47b04c6c3e14256b840880ac0c
geethika <geethikareddy051@gmail.com>
2026-06-29 19:45:30 +0530

    fix: correct import path casing for ThemeContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
-import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ThemeProvider } from "./Context/ThemeContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
